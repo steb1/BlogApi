3bcfb1e4c5bd2303cc28cb20193e90b5ebaa20a2
daoodaba975 <davinhoba9@outlook.fr>
2022-03-28 16:03:17 +0000

    ✅ fix error.

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-express = require('express');
-mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
 const router = require("express").Router();
 const Livre = require("../models/livre");
-app = express();
 
+//mongo db
 const newLocal = "mongodb://127.0.0.1/Livre";
-//Mongodb
-mongoose.connect(newLocal),
-{
-    useNewUrlParser: true, useUnifiedTopology: true,
-},
-() => { 
-    console.log("Connection Reussie")
-}
 
-// middlewares
+mongoose.connect(newLocal),
+    {
+        useNewUrlParser: true, useUnifiedTopology: true,
+    },
+    () => { 
+        console.log("Connection Reussie")
+    }
+
+//middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-//Les routes
+//les routes
 app.use(require("./routes/index"))
 app.use(require("./routes/ajouter"))
 app.use(require("./routes/modifier"))
 app.use(require("./routes/supprimer"))
 app.use(require("./routes/voirlivre"))
 
-
-
-
-
-//Activer le Server
+//activer server
 app.listen(4000, () => console.log("Serveur actif"));
